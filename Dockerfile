a5a515a5bd11174e79b0138d16dab5e4f9fb507e
fidxor <fidxordl5404@gmail.com>
2024-06-13 09:58:02 +0900

    nginx, gunicorn 연동

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /code
 COPY ./requirements.txt /code/
 COPY ./wait-for-it.sh /code/
 RUN apt-get update && \
-apt-get install -y pkg-config default-libmysqlclient-dev build-essential && pip3 install mysqlclient
+apt-get install -y pkg-config default-libmysqlclient-dev build-essential && pip3 install mysqlclient && pip3 install gunicorn
 RUN pip3 install -r requirements.txt
-RUN chmod 777 /code/wait-for-it.sh
+RUN chmod 755 /code/wait-for-it.sh
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-znkl8@l))c%+)=zxwrr0+@d9nxea!mq*%e4!djkx1c$0gz%_)a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["172.23.0.3", "localhost", "127.0.0.1", "172.24.0.4"]
 
 
 # Application definition
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
